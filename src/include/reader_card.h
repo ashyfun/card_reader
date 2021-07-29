3db0a444d67f83cec34237de95c81daedc09c526
k1n9pin <k1n9pin@yandex.ru>
2021-07-30 02:41:20 +0300

    Reverse card number

--- src/include/reader_card.h
+++ src/include/reader_card.h
@@ -8,5 +8,6 @@
 struct reader_tty_t;
 
 char *reader_card_extract(const struct reader_tty_t *rtty);
+void reader_card_reverse(char *card, const int *len);
 
 #endif
--- src/reader_card.c
+++ src/reader_card.c
@@ -15,9 +15,27 @@ char *reader_card_extract(const struct reader_tty_t *rtty)
     }
 
     const int cardlen = end - start;
+
     char *card = malloc(cardlen);
     strncpy(card, rtty->buf + start, end - start);
     card[cardlen] = 0;
 
+    reader_card_reverse(card, &cardlen);
+
     return card;
+}
+
+void reader_card_reverse(char *card, const int *len)
+{
+    char copy[*len];
+    strcpy(copy, card);
+
+    for (int i = *len - 1, j = 0; i >= 0; --i, ++j) {
+        if (j % 2 == 0) {
+            card[j] = copy[i - 1];
+        }
+        else {
+            card[j] = copy[i + 1];
+        }
+    }
 }
